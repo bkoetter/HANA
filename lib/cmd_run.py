3d5bd2b017d40587b968e5cd510e95312e4e7925
Björn Kötter <dev@bk.netburn.de>
2022-04-09 20:15:46 +0200

    comments and flexible handling of return codes

--- lib/cmd_run.py
+++ lib/cmd_run.py
@@ -7,13 +7,20 @@ def main():
     print('This is a module and provides no standalone functionality.')
 
 
-def cmd_run(cmd: str, stdin: bytes = None) -> None:
-    """Execute an OS command with common and trivial error handling. Will terminate program flow in case of error."""
+def cmd_run(cmd: str, stdin: bytes = None, no_exit: tuple = (0,)) -> None:
+    """Execute an OS command with common and trivial error handling. Will terminate program flow in case of error.
+    :param cmd: Command to execute on OS
+    :param stdin: Input to command stdin channel
+    :param no_exit: Do not terminate program flow on these exit codes
+    """
+    if no_exit is None:
+        no_exit = [0, ]
     try:
         run(cmd, input=stdin, check=True, shell=True)
     except CalledProcessError as err:
-        print(f'WARNING: Command "{cmd}" exited with non-zero return code')
-        exit(err.returncode)
+        if err.returncode not in no_exit:
+            print(f'WARNING: Command exited with non-zero return code:\n"{cmd}"')
+            exit(err.returncode)
 
 
 if __name__ == '__main__':
